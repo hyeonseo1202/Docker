2813393485f89268e354c533ad1cb4937f4f92cf
unknown <hhseo1202@naver.com>
2024-11-05 22:58:48 +0900

    feat:docker compose

---
+++ redis-server/server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+
+const PORT = 8080;
+const client = redis.createClient({
+    host: "redis-server",
+    port: 6379
+})
+//APP
+const app = express();
+app.get('/',(req,res)=>{
+    client.get("number", (err, number) => {
+        res.send("Number is going up"+ number);
+        client.set("number",parseInt(number)+1);
+    });
+});
+
+app.listen(PORT);
+console.log("Server is running")
